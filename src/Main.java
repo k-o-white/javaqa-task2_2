c4d3bd85e95ed0625a044f4b43f6830f774e07f6
Kirill Burilichev <koburilichev@gmail.com>
2026-01-21 18:12:20 +0300

    Main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        int balance = 100;
+        System.out.println("Ваш баланс: " + balance + " рублей.");
+        int cashIn = 1100;
+        System.out.println("Пополнение на сумму " + cashIn + " рублей.");
+        balance += cashIn;
+        if (cashIn > 1000) {
+            int bonus = cashIn / 100;
+            System.out.println("Ваш бонус: " + bonus + " рублей.");
+            balance += bonus;
+        }
+        System.out.println("Ваш баланс: " + balance + " рублей.");
     }
 }
